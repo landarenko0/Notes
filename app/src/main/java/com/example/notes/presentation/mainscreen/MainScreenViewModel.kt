e4b3792a0a8c521f0fd3f83a594a63255aadb1ba
landarenko0 <landarenko_nikita@mail.ru>
2024-08-08 20:38:57 +0500

    Refactor

--- app/src/main/java/com/example/notes/presentation/mainscreen/MainScreenViewModel.kt
+++ app/src/main/java/com/example/notes/presentation/mainscreen/MainScreenViewModel.kt
@@ -3,10 +3,10 @@ package com.example.notes.presentation.mainscreen
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.notes.data.room.dao.TaskDao
 import com.example.notes.domain.models.Note
 import com.example.notes.domain.models.Task
 import com.example.notes.domain.usecases.NoteUseCases
+import com.example.notes.domain.usecases.TaskUseCases
 import com.example.notes.util.replaceAllWith
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -14,8 +14,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainScreenViewModel @Inject constructor(
-    private val notesUseCases: NoteUseCases,
-    private val tasksDao: TaskDao
+    private val noteUseCases: NoteUseCases,
+    private val taskUseCases: TaskUseCases
 ) : ViewModel() {
 
     private val _notes = mutableStateListOf<Note>()
@@ -32,32 +32,32 @@ class MainScreenViewModel @Inject constructor(
     }
 
     private suspend fun getAllNotes() {
-        notesUseCases.getAllNotes().collect {
+        noteUseCases.getAllNotes().collect {
             _notes.replaceAllWith(it)
         }
     }
 
     private suspend fun getAllTasks() {
-        tasksDao.getAllTasks().collect {
+        taskUseCases.getAllTasks().collect {
             _tasks.replaceAllWith(it)
         }
     }
 
     fun addNote(note: Note) {
         viewModelScope.launch {
-            notesUseCases.addNote(note)
+            noteUseCases.addNote(note)
         }
     }
 
     fun updateNote(note: Note) {
         viewModelScope.launch {
-            notesUseCases.updateNote(note)
+            noteUseCases.updateNote(note)
         }
     }
 
     fun deleteNotes(notes: List<Note>) {
         viewModelScope.launch {
-            notesUseCases.deleteNotes(notes)
+            noteUseCases.deleteNotes(notes)
         }
     }
 }
